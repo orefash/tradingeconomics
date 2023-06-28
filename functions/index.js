62f042638ebb01219b6ad1c223f6c557f1fefafc
orefash <orefash@gmail.com>
2023-06-28 11:29:31 +0100

    testing cors fix

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,3 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
 const {onRequest} = require("firebase-functions/v2/https");
 
@@ -14,14 +6,22 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-const appRoute = require('./src/app-route');
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+const bodyParser = require("body-parser");
+const cors = require('cors');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors({
 //     origin: "*", // (Whatever your frontend url is) 
-//     credentials: true, // <= Accept credentials (cookies) sent by the client
 // }))
+
+
+app.use(cors({origin: true}));
+
+
+const appRoute = require('./src/app-route');
+
+
+
 
 
 app.use('/',appRoute);
